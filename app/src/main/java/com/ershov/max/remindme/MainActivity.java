35597662265979907ac0de059800054ecfb3e704
Maxim Ershov <xamonn1@gmail.com>
2017-05-27 18:46:56 +0300

    Create main activity

---
+++ app/src/main/java/com/ershov/max/remindme/MainActivity.java
@@ -0,0 +1,17 @@
+package com.ershov.max.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Max on 27.05.2017.
+ */
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
